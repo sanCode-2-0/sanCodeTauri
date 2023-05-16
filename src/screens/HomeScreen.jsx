845a9a8ca5b3fefcb52b5c7da5b71b5eaa37c2a0
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2023-05-16 12:12:43 +0300

    Formatting the HomeScreen component
    Added some formatting to the homescreen component

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,28 +2,38 @@ import "./screens.css";
 
 const HomeScreen = () => {
   return (
-    <div className="row">
-      {/* Main Body Content */}
-      <div className="col l12 section">
-        <h5>Section Main</h5>
-        <p>Some content</p>
+      <div className="row">
+          <div className="container-fluid">
+            {/* Main Body Content */}
+            <div className="col l12 section main">
+                <h5>Choose a category : </h5>
+                <p>Student</p>
+                <p>Staff</p>
 
-        <hr />
-      </div>
+            </div>
 
-      {/* Breadcrumbs section and logo */}
-      <div className="col l8">
-        <nav>
-          <div className="nav-wrapper">
-            <a href="javascript:void(0)" className="breadcrumb">
-              Home
-            </a>
+              {/* Breadcrumbs section and logo */}
+              <div className="sticky-bottom mt-auto">
+              <hr />
+            <div className="col l8 center-align">
+                <nav>
+                    <div className="nav-wrapper">
+                        <a href="#!" className="breadcrumb">
+                            Home
+                        </a>
+                        <a href="#!" className="breadcrumb">
+                             Choose category
+                        </a>
+                    </div>
+                </nav>
+            </div>
+            {/* Logo goes here */}
+              <div className="col l4 right-align">
+                  <img src="../../src/assets/images/lomogan-logo.png" height="65" width="65"/>
+              </div>
+              </div>
           </div>
-        </nav>
       </div>
-      {/* Logo goes here */}
-      <div className="col l4"></div>
-    </div>
   );
 };
 
